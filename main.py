dcd72c655675016ba0f3bef1372495f3b18fa5df
bhaveshhadag <bhaveshgadag46@gmail.com>
2023-07-30 12:04:41 +0530

    plot graph

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from weather_api import get_data
 
 st.title("Weather forecast of any city")
 place = st.text_input("Enter city", key="place")
@@ -6,3 +8,8 @@ days = st.slider("Forecast days", key="days", min_value=1, max_value=5)
 option = st.selectbox("Display option", options=['Temperature', 'Sky'])
 
 st.subheader(f"{option} for next {days} days in {place}")
+
+d, t = get_data(place)
+figure = px.line(x=d, y=t, labels={'x':'Dates', 'y':'Temperatures'})
+
+st.plotly_chart(figure)
---
+++ weather_api.py
@@ -0,0 +1,4 @@
+def get_data(city):
+    dates = ['2023-07-30','2023-07-31', '2023-08-01' ]
+    temperatures = [16, 27, 18]
+    return dates, temperatures
