6c19186ba407cca2a7b356437dad624e330c64b0
bhaveshhadag <bhaveshgadag46@gmail.com>
2023-08-01 21:19:15 +0530

    filter data from api

--- weather_api.py
+++ weather_api.py
@@ -1,4 +1,21 @@
-def get_data(city):
-    dates = ['2023-07-30','2023-07-31', '2023-08-01' ]
-    temperatures = [16, 27, 18]
-    return dates, temperatures
+import requests as r
+
+API_KEY = "72239d9d2567f6e66c8803309b81f901"
+
+def get_data(place, days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+
+    response = r.get(url)
+    data = response.json()['list']
+    if kind == 'Temperature':
+        filtered_data = [dict['main']['temp'] for dict in data]
+    if kind == 'Sky':
+        filtered_data = [dict['weather'][0]['main'] for dict in data]
+    return filtered_data[:8 * days]
+
+
+if __name__ == '__main__':
+    response = get_data("Mumbai", 1, 'Temperature')
+    print(response)
+    response = get_data("Mumbai", 1, 'Sky')
+    print(response)
